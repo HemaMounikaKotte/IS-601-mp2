0321ecc22842ad0222b035bb0bf31789e7cee12c
hema <hemamounikakotte@gmail.com>
2018-10-27 21:31:00 -0400

    This is my Contact Test

---
+++ tests/Feature/ContactTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\User;
+
+class ContactTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testContactPage()
+    {
+        $user = factory(User::class)->create();
+
+        $response = $this->actingAs($user)
+            ->withSession(['foo' => 'bar'])
+            ->get('/contact');
+
+        $response->assertStatus(200);
+    }
+}
