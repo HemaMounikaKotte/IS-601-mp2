12ffa36e5fd3f52c76ad9e9fb8db347e1778ada8
hema <hemamounikakotte@gmail.com>
2018-10-31 23:39:36 -0400

    unit test: This is my update car test - modified

--- tests/Unit/UpdatecarTest.php
+++ tests/Unit/UpdatecarTest.php
@@ -5,19 +5,24 @@ namespace Tests\Unit;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Car;
 
 class UpdatecarTest extends TestCase
 {
     /**
-     * A basic test example.
-     *
-     * @return void
+     This test finds a car with its ID and then updates the car's year to 2000
      */
     public function testUpdatecar()
     {
-        $user = factory(\App\Car::class)->create([
-            'year' => '2000',
-        ]);
+        $cars = Car::all();
+        $car = Car::find(5);
+        $car->year = '2000';
+        $car->save();
         $this->assertDatabaseHas('cars', ["year" => '2000']);
+
+        //$user = factory(\App\Car::class)->create([
+           // 'year' => '2000',
+       // ]);
+
     }
 }
