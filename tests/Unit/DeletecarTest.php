7709fc0dec695c37dd3ca6c414d874bfc5a33eca
hema <hemamounikakotte@gmail.com>
2018-10-31 23:38:31 -0400

    unit test: This is my delete test - modified

--- tests/Unit/DeletecarTest.php
+++ tests/Unit/DeletecarTest.php
@@ -11,16 +11,17 @@ use App\Car;
 class DeletecarTest extends TestCase
 {
     /**
-     * A basic test example.
-     *
-     * @return void
+     This test deletes the cars whose id numbers are more than 50 ,
+     * instead of finding a particular car and deleting it.
+     * if that is the case we can just use the syntax in the cooment section below
      */
     public function testDeletecar()
     {
-        $car = Car::find(51);
+       // $car = Car::find(51);
         //$car-> delete();
         DB::table('cars')->where('id', '>', 50)->delete();
         $cars = Car::all();
         $this->assertDatabaseMissing('cars', ['id' => '51']);
     }
+
 }
